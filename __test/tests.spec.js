3b045cbf705f8c963efa6c9a45649fa21fb27c99
repl.it user <replituser@example.com>
2021-02-23 22:07:33 +0000

    testing calculation object exists

--- __test/tests.spec.js
+++ __test/tests.spec.js
@@ -1,20 +1,34 @@
-const {evaluate} = require('../public/script.js')
+const {evaluate,calculation} = require('../public/script.js')
 
-// Things we can test, i.e. module exports from script.js
-// module.exports = {
-//   screen,
-//   calculation,
-//   append_value,
-//   valid_leadingzeros,
-//   valid_decimals,
-//   trim_invalid_numerics,
-//   control_pressed,
-//   digit_pressed,
-//   operator_pressed,
-//   evaluate
-//   }
+/* Things we can test, i.e. module exports from script.js
+  screen
+ 
+  calculation
+      - push(exp) (adds push to expression)
+  append_value 
+
+  valid_leadingzeros 
+  
+  valid_decimals 
+
+  trim_invalid_numerics
+
+  control_pressed
+
+  digit_pressed
+  
+  operator_pressed,
+  
+  evaluate
+  }
+  */
 
 test('adds 1 + 2 to equal 3', () => {
   expect(evaluate("1+2")).toBe(3);
+});
+
+// testing calculation object
+test('calculation object exists', () => {
+  expect(calculation).toBeDefined;
 });
 
